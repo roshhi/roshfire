92d524e6590eaa5ca0a9398f85d457b5c853700f
MUHAMMAD-AHMAD-07 <not.roshaan@gmail.com>
2025-11-12 07:31:57 +0500

    Completed the weapon category section by adding shotguns,silencers and knifes section.

--- roshfire/src/components/WeaponCategories.jsx
+++ roshfire/src/components/WeaponCategories.jsx
@@ -11,6 +11,26 @@ import rifle3 from '../assets/images/rifle3.png'
 import rifle4 from '../assets/images/rifle4.png'
 import rifle5 from '../assets/images/rifle5.png'
 import rifle6 from '../assets/images/rifle6.png'
+import shotgun1 from '../assets/images/shotgun1.png'
+import shotgun2 from '../assets/images/shotgun2.png'
+import shotgun3 from '../assets/images/shotgun3.png'
+import shotgun4 from '../assets/images/shotgun4.png'
+import shotgun5 from '../assets/images/shotgun5.png'
+import shotgun6 from '../assets/images/shotgun6.png'
+import silencer1 from '../assets/images/s1.png'
+import silencer2 from '../assets/images/s2.png'
+import silencer3 from '../assets/images/s3.png'
+import silencer4 from '../assets/images/s4.png'
+import silencer5 from '../assets/images/s5.png'
+import silencer6 from '../assets/images/s6.png'
+import knife1 from '../assets/images/k1.png'
+import knife2 from '../assets/images/k2.png'
+import knife3 from '../assets/images/k3.png'
+import knife4 from '../assets/images/k4.png'
+import knife5 from '../assets/images/k5.png'
+import knife6 from '../assets/images/k6.png'
+
+
 
 export default function WeaponCategories(){
     
@@ -25,22 +45,46 @@ export default function WeaponCategories(){
     const pistols = [
         {src: pistol1, name:'BERETTA M9'},
         {src: pistol2, name:'COLT 1911'},
-        {src: pistol3, name:'REVOLVER 686'},
+        {src: pistol3, name:'REVOLVER'},
         {src: pistol4, name:'BERETTA 92'},
         {src: pistol5, name:'GLOCK 17'},
         {src: pistol6, name:'GLOCK 19'},
     ];
     const rifles = [
-        {src: rifle1, name:'BERETTA M9'},
-        {src: rifle2, name:'COLT 1911'},
-        {src: rifle3, name:'REVOLVER 686'},
-        {src: rifle4, name:'BERETTA 92'},
-        {src: rifle5, name:'GLOCK 17'},
-        {src: rifle6, name:'GLOCK 19'},
+        {src: rifle1, name:'M4 CARBINE'},
+        {src: rifle2, name:'AK-47'},
+        {src: rifle3, name:'HK G36'},
+        {src: rifle4, name:'MK 18'},
+        {src: rifle5, name:'SAIGA MK'},
+        {src: rifle6, name:'CZ SCORPION'},
+    ];
+    const shotguns = [
+        {src: shotgun1, name:'MOSSBERG 590'},
+        {src: shotgun2, name:'REMINGTON 870'},
+        {src: shotgun3, name:'WINCHESTER SXP'},
+        {src: shotgun4, name:'MAVERICK 88'},
+        {src: shotgun5, name:'WINGMASTER 870'},
+        {src: shotgun6, name:'WINCHESTER MARINE'},
+    ];
+    const silencers = [
+        {src: silencer1, name:'OSPREY'},
+        {src: silencer2, name:'OBSIDION'},
+        {src: silencer3, name:'WOLFMAN'},
+        {src: silencer4, name:'ERECTOR 9'},
+        {src: silencer5, name:'BANISH 45'},
+        {src: silencer6, name:'TI-RANT'},
+    ];
+    const knifes = [
+        {src: knife1, name:'KA-BAR'},
+        {src: knife2, name:'FAIRBAIRN'},
+        {src: knife3, name:'KUKRI'},
+        {src: knife4, name:'GERBER MARK'},
+        {src: knife5, name:'STILETTO'},
+        {src: knife6, name:'YARBOROUGH'},
     ];
 
     return (
-        <div className="relative bottom-[50px]">
+        <div className="relative bottom-[60px] md:bottom-[70px] lg:bottom-[80px] xl:bottom-[100px]">
             <div className="flex items-center justify-center flex-col w-[90%] mx-auto bg-white">
                 <div className="grid grid-cols-5 place-items-center w-[100%]  text-white extraSmallResFont h-[35px] font-medium">
                     {
@@ -57,13 +101,13 @@ export default function WeaponCategories(){
                         })
                     }
                 </div>
-                <div className="weaponGrid grid grid-cols-3 gap-4 ml-4 bg-white pt-5 lg:grid-cols-6">
+                <div className="weaponGrid grid grid-cols-3 gap-4 ml-4 bg-white pt-5 lg:grid-cols-6 mt-5">
                     {
                         selected === "handgun" && pistols.map((pistol)=>{
                             return(
-                                <div key={pistol.name} className="grid place-items-center border">
+                                <div key={pistol.name} className="grid place-items-center border-2 hover:border-yellow-500 transform hover:translate-y-[-10px] transition-transform duration-300 hover:font-bold">
                                     <img src={pistol.src} alt={pistol.name + " image"} />
-                                    <p>{pistol.name}</p>
+                                    <p className="smallResFont mt-5">{pistol.name}</p>
                                 </div>
                             )
                         })
@@ -71,9 +115,39 @@ export default function WeaponCategories(){
                     {
                         selected === "rifle" && rifles.map((rifle)=>{
                             return(
-                                <div key={rifle.name} className="grid place-items-center border">
+                                <div key={rifle.name} className="grid place-items-center border-2 hover:border-yellow-500 transform hover:translate-y-[-10px] transition-transform duration-300 hover:font-bold">
                                     <img src={rifle.src} alt={rifle.name + " image"} />
-                                    <p>{rifle.name}</p>
+                                    <p className="smallResFont mt-5">{rifle.name}</p>
+                                </div>
+                            )
+                        })
+                    }
+                    {
+                        selected === "shotguns" && shotguns.map((shotgun)=>{
+                            return(
+                                <div key={shotgun.name} className="grid place-items-center border-2 hover:border-yellow-500 transform hover:translate-y-[-10px] transition-transform duration-300 hover:font-bold">
+                                    <img src={shotgun.src} alt={shotgun.name + " image"} />
+                                    <p className="smallResFont mt-5">{shotgun.name}</p>
+                                </div>
+                            )
+                        })
+                    }
+                    {
+                        selected === "silencers" && silencers.map((silencer)=>{
+                            return(
+                                <div key={silencer.name} className="grid place-items-center border-2 hover:border-yellow-500 transform hover:translate-y-[-10px] transition-transform duration-300 hover:font-bold">
+                                    <img className="!p-5" src={silencer.src} alt={silencer.name + " image"} />
+                                    <p className="smallResFont mt-5">{silencer.name}</p>
+                                </div>
+                            )
+                        })
+                    }
+                    {
+                        selected === "knives" && knifes.map((knife)=>{
+                            return(
+                                <div key={knife.name} className="grid place-items-center border-2 hover:border-yellow-500 transform hover:translate-y-[-10px] transition-transform duration-300 hover:font-bold">
+                                    <img className="!p-5" src={knife.src} alt={knife.name + " image"} />
+                                    <p className="smallResFont mt-5">{knife.name}</p>
                                 </div>
                             )
                         })
