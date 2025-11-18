0135f287d4f1b8759d3a70034658d55b0258b5a7
MUHAMMAD-AHMAD-07 <not.roshaan@gmail.com>
2025-11-18 06:45:57 +0500

    Created a custom hook to fetch data from firebase,to check auth state, to increase/decrease cart items quantity in firestore db, to set loader and manage local cart state.

---
+++ frontend/src/hooks/useCart.js
@@ -0,0 +1,105 @@
+import { db,auth } from "../firebase/firbaseConfigFile";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect,useState } from "react";
+import { doc,deleteDoc,setDoc,collection,getDocs } from "firebase/firestore";
+
+export default function useCart(){
+
+    const [user, setUser] = useState(null);
+    const [cart, setCart] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+
+    // Get user
+    useEffect(() => {
+        const unsub = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            if (currentUser) fetchCart(currentUser.uid);
+            else{
+                setCart([]);
+                setLoading(false);
+            }
+        });
+        return () => unsub();
+    }, []);
+
+    // Fetch cart from Firestore
+    const fetchCart = async (userId) => {
+        setLoading(true);
+
+        const cartRef = collection(db, "users", userId, "cartItems");
+        const allDocs = await getDocs(cartRef);
+        
+        const items = allDocs.docs.map((doc) => doc.data());
+        setCart(items);
+
+        setLoading(false);
+    };
+
+    // Add first time
+    const addItem = async (image, title, price) => {
+        if (!user) return;
+        const ref = doc(db, "users", user.uid, "cartItems", title);
+        await setDoc(ref, {
+            image,
+            title,
+            price,
+            qty: 1,
+        });
+        setCart((prev) => [...prev, { image, title, price, qty: 1 }]); // takes the old array and add new one
+    };
+
+    // Increase qty
+    const increaseQty = async (title) => {
+        const item = cart.find((i) => i.title === title);
+        if (!item || !user) return;
+
+        const ref = doc(db, "users", user.uid, "cartItems", title);
+
+        await setDoc(ref, {
+        ...item,
+        qty: item.qty + 1,
+        });
+
+        setCart((prev) =>
+        prev.map((i) => (i.title === title ? { ...i, qty: i.qty + 1 } : i))
+        );
+    }
+
+    // Decrease qty
+    const decreaseQty = async (title) => {
+        const item = cart.find((i) => i.title === title);
+        if (!item || !user) return;
+
+        const ref = doc(db, "users", user.uid, "cartItems", title);
+
+        if (item.qty === 1) {
+            await deleteDoc(ref);
+            setCart((prev) => prev.filter((i) => i.title !== title));
+            return;
+        }
+        await setDoc(ref, {
+            ...item,
+            qty: item.qty - 1,
+        });
+
+        setCart((prev) =>
+        prev.map((i) => (i.title === title ? { ...i, qty: i.qty - 1 } : i))
+        );
+    }
+
+
+    // Delete whole item
+    const removeItem = async (title) => {
+        if (!user) return;
+
+        const ref = doc(db, "users", user.uid, "cartItems", title);
+        await deleteDoc(ref);
+
+        setCart((prev) => prev.filter((i) => i.title !== title));
+    };
+
+    return{
+        user,cart,addItem,increaseQty,decreaseQty,removeItem,loading
+    }
+}
